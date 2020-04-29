548ace3ba472aaa0bd433aee241ec00b7c6e5b95
Madalin <zornet.samp@gmail.com>
2020-04-29 17:57:41 +0300

    added lab9 gRPC and EF

---
+++ gRPC_si_EF/GrpcService/Interface.cs
@@ -0,0 +1,52 @@
+﻿using DataBasePostComment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace GrpcService
+{
+    [ServiceContract]
+    interface Interface
+    {
+        [OperationContract]
+        bool AddPost(Post post);
+        [OperationContract]
+        Post UpdatePost(Post post);
+        [OperationContract]
+        int DeletePost(int id);
+        [OperationContract]
+        Post GetPostById(int id);
+        [OperationContract]
+        List<Post> GetPosts();
+    }
+    [ServiceContract]
+    interface InterfaceComment
+    {
+        [OperationContract]
+        bool AddComment(Comment comment);
+        [OperationContract]
+        Comment UpdateComment(Comment newComment);
+        [OperationContract]
+        Comment GetCommentById(int id);
+    }
+    [ServiceContract]
+    interface InterfacePost
+    {
+        [OperationContract]
+        bool AddPost(Post post);
+        [OperationContract]
+        Post UpdatePost(Post post);
+        [OperationContract]
+        int DeletePost(int id);
+        [OperationContract]
+        Post GetPostById(int id);
+        [OperationContract]
+        List<Post> GetPosts();
+    }
+    [ServiceContract]
+    interface IPostComment : InterfacePost, InterfaceComment
+    {
+    }
+}
